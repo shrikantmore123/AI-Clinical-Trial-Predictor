437b27995f42a6f4a3a62c4079d27c08d7c60f80
Shrikant More <shrikantmore008@gmail.com>
2025-10-09 19:37:54 +0530

    Implemented MVC Arcgitecture

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const ejsMate = require("ejs-mate");
+const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+
+const userRouter = require("./routes/users.js");
 
 const port = 3000;
 const MONGO_URL = "mongodb://127.0.0.1:27017/clinical_trial";
@@ -20,18 +28,40 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true })); // ✅ important
+
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currUser = req.user;
+  next();
+});
 
 app.get("/", (req, res) => {
   res.render("templates/index");
 });
 
-app.get("/login", (req, res) => {
-  res.render("users/login");
-});
-
-app.get("/signup", (req, res) => {
-  res.render("users/signup");
-});
+app.use("/", userRouter);
 
 app.get("/form", (req, res) => {
   res.render("templates/form");
@@ -41,8 +71,13 @@ app.get("/dashboard", (req, res) => {
   res.render("templates/dashboard");
 });
 
-app.get("*", (req, res) => {
-  res.status(404).send("404 | Page Not Found");
+app.all("*", (req, res, next) => {
+  next(new ExpressError(404, "Page Not Found"));
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  res.status(statusCode).render("error.ejs", { err });
 });
 
 app.listen(port, () => {
