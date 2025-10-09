586cc6cc49c21c9cb9d820e2794b15362180b2ea
Shrikant More <shrikantmore008@gmail.com>
2025-10-09 13:37:49 +0530

    Home page and authentication pages added

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+const mongoose = require("mongoose");
+const path = require("path");
+const ejsMate = require("ejs-mate");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const port = 3000;
+const MONGO_URL = "mongodb://127.0.0.1:27017/clinical_trial";
+
+main()
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.log("❌ DB Connection Error:", err));
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("templates/index");
 });
 
-app.get('*', (req, res) => {
-  res.send('Error page')
+app.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("users/signup");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("404 | Page Not Found");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`🚀 Server running at http://localhost:${port}`);
 });
